d8de6f1ec2988b955a6fffea3772cc5d2b34c188
AlexeyPermyakov <apermyakov@news360.ru>
2015-07-01 13:29:27 +0500

    Add more material

--- src/app/startScreen/StartScreenController.js
+++ src/app/startScreen/StartScreenController.js
@@ -1,5 +1,5 @@
-angular.module('toywheel_admin').controller('StartScreenController', ['$route', '$routeParams',
+toywheel_admin.controller('StartScreenController', ['$route', '$routeParams',
     function($route, $routeParams, $scope) {
-        console.log('StartScreenController');
+
     }]
 );
